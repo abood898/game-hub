6617ae3ee6e7e745c513dfd62ad3f7c733479af3
abood898 <aboodnahhas84@gmail.com>
2023-04-29 21:40:53 +0300

    Install Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Button colorScheme="blue">Button</Button>
+    </div>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.css";
+import { ChakraProvider } from "@chakra-ui/react";
+// import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
