aa7fe246a9dd26cd1c53ccff67d4d3240fe54389
abood898 <aboodnahhas84@gmail.com>
2023-05-02 02:34:39 +0300

    Refactor Game Grid

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-// import "bootstrap/dist/css/bootstrap.css";
 import theme from "./theme";
 import "./index.css";
 import App from "./App";
